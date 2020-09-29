63c2b498b6ca27bbd47a8a543ac45a530826cd58
mguinness <sufc@netzero.net>
2020-09-28 20:06:27 -0700

    Security advisory fixes

--- IdentityManager/Models/ManageViewModels/EnableAuthenticatorViewModel.cs
+++ IdentityManager/Models/ManageViewModels/EnableAuthenticatorViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
@@ -9,15 +10,16 @@ namespace IdentityManager.Models.ManageViewModels
 {
     public class EnableAuthenticatorViewModel
     {
-            [Required]
-            [StringLength(7, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-            [DataType(DataType.Text)]
-            [Display(Name = "Verification Code")]
-            public string Code { get; set; }
+        [Required]
+        [StringLength(7, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Verification Code")]
+        public string Code { get; set; }
 
-            [ReadOnly(true)]
-            public string SharedKey { get; set; }
+        [BindNever]
+        public string SharedKey { get; set; }
 
-            public string AuthenticatorUri { get; set; }
+        [BindNever]
+        public string AuthenticatorUri { get; set; }
     }
 }
